564ce28ccef6880bd11fd0098e10e9c6f8cdc771
smallAbyss <eal1105@yandex.ru>
2026-05-15 03:05:21 +0300

    [DEV] split media

--- scp.cpp
+++ scp.cpp
@@ -5,9 +5,36 @@
 #include <filesystem> 
 #include <algorithm>
 #include <set>
+#include <tuple>
+
 
 
 namespace fs = std::filesystem;
+using VoS = std::vector<std::string>;
+using ToVoS = std::tuple<VoS, VoS, VoS>;
+
+const std::set<std::string> IMAGE_EXTS = {
+    ".jpg", ".jpeg", ".png", ".bmp", ".webp"
+};
+const std::set<std::string> VIDEO_EXTS = {
+    ".mp4", ".avi", ".mkv", ".mov", ".flv", ".webm"
+};
+const std::set<std::string> AUDIO_EXTS = {
+    ".mp3", ".wav", ".flac", ".ogg"
+};
+const std::set<std::string> C_EXTS = {
+    ".c"
+};
+
+
+
+std::string whichMedia(const std::string& ext) {
+    if (AUDIO_EXTS.find(ext) != AUDIO_EXTS.end()) return "au";
+    if (C_EXTS.find(ext) != C_EXTS.end()) return "c";
+    if (IMAGE_EXTS.find(ext) != IMAGE_EXTS.end()) return "pic";
+    if (VIDEO_EXTS.find(ext) != VIDEO_EXTS.end()) return "vid";
+    return "idk";
+}
 
 bool isMedia(const fs::path& path) {
      std::string ext = path.extension().string();
@@ -23,51 +50,93 @@ bool isMedia(const fs::path& path) {
     return extensions.find(ext) != extensions.end();
 }
 
-
-std::vector<std::string> scanDirectory(const std::string& path) {
-    std::vector<std::string> files;
+ToVoS scanDirectory(const std::string& path) {
+    std::vector<std::string> images;
+    std::vector<std::string> videos;
+    std::vector<std::string> audios;
     try {
         for (const auto& entry : fs::recursive_directory_iterator(path, fs::directory_options::skip_permission_denied)) {
             if (entry.is_regular_file() && isMedia(entry.path())) {
-                files.push_back(entry.path().string());
-                std::cout << entry.path() << std::endl;
-
+                std::string type = whichMedia(entry.path().extension().string()); 
+                if (type == "pic") 
+                    images.push_back(entry.path().string());
+                if (type == "vid")
+                    videos.push_back(entry.path().string());
+                if (type == "au")
+                    audios.push_back(entry.path().string());
+                if (type == "c")
+                    audios.push_back(entry.path().string());
             }
         }
     }
     catch (...) {
         ;
     }
-    return files;
+    return std::make_tuple(videos, images, audios);
 }
 
-void saveToJson(const std::vector<std::string>& files, const std::string& output_file) {
+
+void saveToJson(const ToVoS& files, const std::string& output_file) {
     std::ofstream file(output_file);
-    
+    VoS audios = std::get<2>(files);
+    VoS images = std::get<1>(files);
+    VoS videos = std::get<0>(files);
+
     // creating JSOn
-    file << "{\n" << "\"audio\":[\n";
-    for (size_t i = 0; i < files.size(); ++i) {
-        file << "    \"" << files[i] << "\"";
+    file << "{\n";
+    file << "\"audio\":[\n";
+    for (size_t i = 0; i < audios.size(); ++i) {
+        file << "    \"" << audios[i] << "\"";
         
-        if (i < files.size() - 1) {
+        if (i < audios.size() - 1) {
+            file << ",";
+        }
+        file << "\n";
+    }
+    file << "  ],\n";
+
+    file << "\"images\":[\n";
+    for (size_t i = 0; i < images.size(); ++i) {
+        file << "    \"" << images[i] << "\"";
+        
+        if (i < images.size() - 1) {
+            file << ",";
+        }
+        file << "\n";
+    }
+    file << "  ],\n";
+
+    file << "\"videos\":[\n";
+    for (size_t i = 0; i < videos.size(); ++i) {
+        file << "    \"" << videos[i] << "\"";
+        
+        if (i < videos.size() - 1) {
             file << ",";
         }
         file << "\n";
     }
     file << "  ]\n";
+
     file << "}\n";
     file.close();
 }
 
 
+unsigned sizeOfTovos(const ToVoS& var) {
+    VoS audios = std::get<2>(var);
+    VoS images = std::get<1>(var);
+    VoS videos = std::get<0>(var);
+}
+
 int main() {
     const char* home = std::getenv("HOME");
 
-    std::vector<std::string> media_files = scanDirectory(home);
+    auto media_files = scanDirectory(home);
     
     saveToJson(media_files, "media_files.json");
     
-    std::cout << "Found: " << media_files.size() << "\n";
+
+    // std::cout << "Found: " << media_files.size() << "\n";
     
     return 0;
 }
